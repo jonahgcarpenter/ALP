af1de89d07c5513fe7fcbb64ca5443c89f33034c
Jonah Carpenter <jonah4chiefs@gmail.com>
2024-12-10 12:32:52 -0600

    using tailwind css for easier styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,11 @@
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-import Navbar from "./components/navbar";
-import About from "./pages/About";
-import Puppies from "./pages/Puppies";
-import Family from "./pages/Family";
-import Media from "./pages/Media";
-import { AuthProvider } from "./auth/AuthContext";
+import Nav from '../src/components/Navbar';
 
 const App: React.FC = () => {
   return (
-    <AuthProvider>
-      <div>
-        <Navbar />
-        <main>
-          <Routes>
-            <Route path="/about" element={<About />} />
-            <Route path="/puppies" element={<Puppies />} />
-            <Route path="/family" element={<Family />} />
-            <Route path="/media" element={<Media />} />
-            <Route path="/" element={<Navigate to="/about" />} />
-          </Routes>
-        </main>
-      </div>
-    </AuthProvider>
+      <>
+        <Nav />
+      </>
   );
 };
 
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Nav from '../components/Navbar';
+
+const HomePage: React.FC = () => {
+  return (
+    <main className="page">
+      <Nav />
+    </main>
+  );
+};
+
+export default HomePage;
